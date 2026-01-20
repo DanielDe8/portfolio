ddc7bbdc8bc40b9e75037686f32066c910b84830
DanikD <daniel.test800@gmail.com>
2026-01-20 05:09:58 +0100

    add skills

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
     import Project from "./components/Project.svelte"
+    import Skill from "./components/Skill.svelte"
 </script>
 
 <div class="bg-base-200 min-h-screen">
@@ -37,9 +38,11 @@
 
             <Project
                 name="xo"
-                description='Online hra piškvorky s neomezeným polem a možností vytvoření účtu, napsaná v jazyce JavaScript s frameworky SvelteKit, Tailwind a DaisyUI na straně klienta a Node.js, Socket.io a Express na straně serveru, který běhá na Raspberry Pi v mém pokoji. <br>
+                description='Online hra piškvorky s neomezeným polem a možností vytvoření účtu. Napsaná v jazyce JavaScript s frameworky SvelteKit, Tailwind a DaisyUI na straně klienta a Node.js, Socket.io a Express na straně serveru.
+                        Běhá buď na Raspberry Pi v mém pokoji, nebo na Dropletu v <a class="underline text-accent" href="https:/digitalocean.com" target="_blank">DigitalOcean</a>. <br>
                         Základní kostra HTML byla vygenerována pomocí AI, vzhled stránce však dává DaisyUI. <br>
                         Jde hrát proti náhodému soupeři, nebo proti přátelům pomocí unikátního odkazu. <br>
+                        Hra běží na adrese: <a class="underline text-primary" href="https://xo.dandev.dev" target="_blank">xo.dandev.dev</a> <br>
                         Zdrojový kód pro server je dostupný na GitHubu: <a class="underline text-primary" href="https://github.com/danielde8/xo-server" target="_blank">danielde8/xo-server</a>,
                         pro klienta na: <a class="underline text-primary" href="https://github.com/danielde8/xo-frontend" target="_blank">danielde8/xo-frontend</a>'
                 imgSrc="assets/projects/xo.png"
@@ -90,9 +93,44 @@
 
             <div class="p-15 bg-base-300">
                 <h2 class="text-6xl font-semibold">Dovednosti</h2>
-                <p class="text-3xl mt-7">
-                    
-                </p>
+
+                <Skill
+                    name="JavaScript (Frontend)"
+                    description='JavaScriptu se věnuji od 3. třídy. Začínal jsem s jednoduchým kreslením v aplikaci <a class="underline text-accent" href="https://p5js.org/" target="_blank">p5.js</a>,
+                        později jsem zkoušel i tvorbu webových stránek a her. Před nedávnem jsem objevil framework <a class="underline text-accent" href="https://svelte.dev/" target="_blank">Svelte</a>,
+                        který mě velmi zaujal, hlavně svou jednoduchostí (doufám že jednoho dne přeroste React :D). Dnes hlavně používám Svelte spolu s Tailwind CSS a DaisyUI, ve kterých byla napsána i tato stránka.' 
+                />
+
+                <Skill
+                    name="Node.js"
+                    description='Node.js používám pro tvorbu backendů/API pro své webové aplikace. Pro API nebo statické stránky používám framework <a class="underline text-accent" href="https://expressjs.com" target="_blank">Express.js</a>
+                    hlavně kvůli jeho jednoduchosti a komunitě. Pro real-time komunikaci (např. ve hrách) používám <a class="underline text-accent" href="https://socket.io/" target="_blank">Socket.io</a>.
+                    Node.js mám velmi rád, protože mi umožňuje psát frontend i backend ve stejném jazyce. Naučil jsem se na něm mimo jiné i základy HTTP requestů.'
+                />
+
+                <Skill
+                    name="Kotlin"
+                    description='Ve 4. třídě jsem se začal učit programovat v jazyce Kotlin, ne však pro Android, ale pro tvorbu her a desktopových aplikací
+                    pomocí <a class="underline text-accent" href="https://korge.org" target="_blank">KorGE</a> a Java Swing.
+                    Kotlin je dnes můj nejoblíbenější jazyk, hlavně pro jeho všechny možné vestavěné funkce a možnost využít Java knihovny.
+                    V Kotlinu většinou píšu menší projekty, které bývají spíše experimenty než hotové aplikace.'
+                />
+
+                <Skill
+                    name="C a Assembly"
+                    description='Sice o sobě nemohu říct, že jsem v těchto jazycích expert, ale něco v nich napsat dokážu.
+                    Už dlouho byl můj sen vytvořit vlastní operační systém, který by se dal spustit alespoň na VM, i kdyby to mělo být jen <span class="italic">Hello World</span>.
+                    To se mi konečně podařilo někdy v 6. třídě, kdy jsem (pomocí AI :D) napsal jednoduchý Assembly kód, který vypsal jedno písmeno a dal se spustit ve <a class="underline text-accent" href="https://www.virtualbox.org/" target="_blank">VirtualBoxu</a>.
+                    Po snad ještě delší době se mi <span class="italic">konečně</span> podařilo načíst C program a zanedlouho nato jsem začal pracovat na svém vlastním "OS", který je popsán výše.<br>
+                    C mimo to používám i pro psaní programů, kde potřebuji často pracovat s čísly na nižší úrovni, například pro emulátor CPU. (MUPC - viz výše)'
+                />
+
+                <Skill
+                    name="Linux a servery"
+                    description='Už ani nevím, kdy jsem začal používat Linux, ale je to už celkem dávno.
+                    Většinou používám distribuci Ubuntu Server nebo Kali Linux. Na svém hlavním počítači mám sice macOS, ale mám i dva staré notebooky, které jsem přeinstaloval z Windows 7 na Linux.
+                    První jsem používal jako Minecraft server pro kamarády, na druhém běží Kali, používám ho tedy na učení se KB, hlavně na <a class="underline text-accent" href="https://www.hackthebox.com/" target="_blank">Hack The Box</a>.'
+                />
             </div>
         </div>
     </div>
---
+++ src/components/Skill.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let name
+    export let description
+</script>
+
+<div class="m-7">
+    <h3 class="text-5xl font-semibold">{name}</h3>
+    <p class="text-2xl m-2">{@html description}</p>
+</div>
